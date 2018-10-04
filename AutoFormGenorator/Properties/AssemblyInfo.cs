67ba42049fb28b15ba01768be7a42eddce2c1bbc
Justin - Echo <justin@jnet.systems>
2018-10-04 22:33:21 +1300

    -Adding Author

--- AutoFormGenorator/Properties/AssemblyInfo.cs
+++ AutoFormGenorator/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AutoFormGenorator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Justin Campbell")]
 [assembly: AssemblyProduct("AutoFormGenorator")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
