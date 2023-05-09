29b0d96617e1f4cf6a34d0c5dcdcded3da1f19b9
Justinc <Justinc@ruapehudc.govt.nz>
2023-05-10 11:19:40 +1200

    release 1.7.3

--- AutoFormGenorator/Properties/AssemblyInfo.cs
+++ AutoFormGenorator/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("JnetSystems")]
 [assembly: AssemblyProduct("AutoFormGenerator")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.2.0")]
+[assembly: AssemblyVersion("1.7.3.0")]
